e8d7e658c8d1eea6b3bd0b462ab9d4bd56a9cc78
kashichavan <kashichavan7777@gmail.com>
2025-11-18 15:31:32 +0530

    finalDemo

--- myprocrud/settings.py
+++ myprocrud/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = 'myprocrud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'XvCRsWHaiJAlIUNwiMYIwzIlMBkcUthx',
+        'HOST': 'shuttle.proxy.rlwy.net',
+        'PORT': '11667',
     }
 }
+
 
 
 # Password validation
