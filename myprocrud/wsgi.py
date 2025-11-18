2d3aa4e46511d2f78b214e73a3f85b6fa44df21e
kashichavan <kashichavan7777@gmail.com>
2025-11-18 15:03:22 +0530

    finalDOne

--- myprocrud/wsgi.py
+++ myprocrud/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myprocrud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mynetcrud.settings')
 
 application = get_wsgi_application()
+
+app=application
